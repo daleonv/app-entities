610e7e44a25ebfc6d60567439c2ac82e953a69c8
daleonv <daleon@ec.krugercorp.com>
2025-02-13 15:33:39 -0500

    Identación de código

--- src/main/java/com/ec/app/entities/procedures/CustomerEntity.java
+++ src/main/java/com/ec/app/entities/procedures/CustomerEntity.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,7 +12,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.io.Serializable;
 import java.util.List;
 
 @Data
@@ -22,7 +20,7 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "Customer", schema = "technical-test")
-public class CustomerEntity extends PersonEntity{
+public class CustomerEntity extends PersonEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
